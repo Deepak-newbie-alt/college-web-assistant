c51695481c7b8919ef39f7362bd56e86ffb7328e
Deepak <dk6625901@gmail.com>
2025-10-20 00:23:55 +0530

    fix: netlify build error and node version set

--- FRONTEND/src/api/axios.js
+++ FRONTEND/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',  // change to your backend URL
+  baseURL: 'https://college-web-assistant.onrender.com/api',
 });
 
 // Attach token from localStorage to each request
